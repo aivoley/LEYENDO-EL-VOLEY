b0d43495f3be99d8eb5d31e9c561263326893dcc
aivoley <aivoley2@gmail.com>
2025-04-26 15:57:11 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
-import type {NextConfig} from 'next';
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+module.exports = nextConfig;
 
-export default nextConfig;
+
+
